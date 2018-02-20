f9eb324d1f3a8592115b17fa533f01271722da75
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-02-21 02:12:52 +0530

    Working on the homepage feed thing. man i need sleep

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var PORT = 3000
 
-app.get("/", function(req, res){
-	res.render("<html><body><h1>Hello world</h1><body></html>");
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+app.get("/*", function(req, res){
+	res.render("home");
 });
 
 
