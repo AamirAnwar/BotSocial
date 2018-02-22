96ceb28798ce741b8c69ed2c3630a0eea8f745ba
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-02-22 14:40:44 +0530

    Added user object reference to a story

--- models/story.js
+++ models/story.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 var StorySchema = new mongoose.Schema({
 	text:String,
-	date:{type:Date,default:Date.now}
+	date:{type:Date,default:Date.now},
+	author:{
+		id: {
+			type:mongoose.Schema.Types.ObjectId,
+			ref:'User'
+		},
+		username:String
+	}
 });
 module.exports =  mongoose.model("Story", StorySchema);
--- routes/story.js
+++ routes/story.js
@@ -11,6 +11,10 @@ router.post("/",middleware.isLoggedIn,function(req,res){
 			console.log(err);
 		}
 		else {
+			console.log("Current user is" + req.user);
+			story.author.id = req.user._id;
+			story.author.username = req.user.username;
+			story.save();
 			console.log(story);
 		}
 		res.redirect("/");
